aed9bf2e0d8e88e91a5e3d86c76d06d0703363cc
abdurrahman026 <adrahman117@gmail.com>
2025-07-01 20:17:38 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Counter from "./components/Counter";
+import Form from "./components/Form";
+import ProductList from "./components/ProductList";
 import Team from "./components/TeamMembers";
+import Temperature from "./components/Temp";
+
+import Todo from "./components/Todo";
 
 export default function App(){
     return (
         <>
-        <Counter/>
+      <Temperature/>
         </>
     );
 }
